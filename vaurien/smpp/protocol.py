b04f0cd0496d73e13dae62fcf5b36639bb3dd90c
Chad Estioco <chadestioco@gmail.com>
2014-06-17 16:52:02 +0800

    Initial commit for source directory. Till I realized I need Twisted.

---
+++ vaurien/smpp/protocol.py
@@ -0,0 +1,15 @@
+import vaurien.protocols.base.BaseProtocol
+import smpp.pdu
+
+"""
+SMPP Protocol handling for Vaurien. Bulk of the work is done by smpp.pdu.
+
+http://vaurien.readthedocs.org/en/1.8/extending.html#extending
+"""
+
+class SMPP(BaseProtocol):
+    name = "smpp"
+    options = {"port": {"Port where vaurien will bind", int, 16981},}
+
+    def _handle(self, source, dest, to_backend):
+        data = self._get_data(source)
