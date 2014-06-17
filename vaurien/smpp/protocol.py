fc0a976faebabec275c3d1b7bb4788d43ccd0592
Chad Estioco <chadestioco@gmail.com>
2014-06-17 17:23:47 +0800

    Some more work on it...

--- vaurien/smpp/protocol.py
+++ vaurien/smpp/protocol.py
@@ -1,3 +1,5 @@
+from twisted.internet.protocol import Protocol
+
 import vaurien.protocols.base.BaseProtocol
 import smpp.pdu
 
@@ -7,9 +9,13 @@ SMPP Protocol handling for Vaurien. Bulk of the work is done by smpp.pdu.
 http://vaurien.readthedocs.org/en/1.8/extending.html#extending
 """
 
-class SMPP(BaseProtocol):
+class SMPP(BaseProtocol, Protocol):
+    """
+    Extends both BaseProtocol from Vaurien and Protocol from Twisted.
+    """
+
     name = "smpp"
-    options = {"port": {"Port where vaurien will bind", int, 16981},}
+    options = {"port": ("Port where vaurien will bind", int, 16981),}
 
     def _handle(self, source, dest, to_backend):
         data = self._get_data(source)
