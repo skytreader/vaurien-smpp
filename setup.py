fc4842c7e3bb2d048d3a2c7528d0c46a8678ed0a
Chad Estioco <chadestioco@gmail.com>
2014-06-17 16:28:30 +0800

    Boilerplate setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = "vaurien-smpp",
+    version = "0.1.0",
+    author = "Chad Estioco",
+    author_email = "chadestioco@gmail.com",
+    url = "https://github.com/skytreader/vaurien-smpp",
+    packages = ["vaurien.smpp",],
+    install_requires = ["vaurien", "smpp.pdu"],
+    license="MIT",
+    description = "SMPP extension for Vaurien.",
+)
